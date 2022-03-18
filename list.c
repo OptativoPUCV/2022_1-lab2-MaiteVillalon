5ab435b3dc30b803a902ad993c6a3dd36fe1ff88
Your Name <you@example.com>
2022-03-18 20:31:27 -0300

    new try

--- list.c
+++ list.c
@@ -19,36 +19,71 @@ struct List {
 
 typedef List List;
 
-Node * createNode(void * data) {
-    Node * new = (Node *)malloc(sizeof(Node));
-    assert(new != NULL);
-    new->data = data;
-    new->prev = NULL;
-    new->next = NULL;
-    return new;
+Node * createNode(void * data)
+{
+    Node * nodo = (Node *)malloc(sizeof(Node));
+    nodo->data = data;
+    nodo->prev = NULL;
+    nodo->next = NULL;
+    return nodo;
 }
 
-List * createList() {
-     return NULL;
+List * createList() 
+{
+    List * listaNueva = (List *) malloc (sizeof(List));
+    listaNueva->head = NULL;
+    listaNueva->current = NULL;
+    listaNueva->tail = NULL;
+    return listaNueva;
+
 }
 
-void * firstList(List * list) {
-    return NULL;
+void * firstList(List * list) 
+{
+    if (!lista->head) return NULL;
+    lista->current=lista->head;
+
+    return lista->head->data;
 }
 
-void * nextList(List * list) {
-    return NULL;
+void * nextList(List * list)
+{
+    if (!lista->current || !lista->current->next ) return NULL;
+    lista->current = lista->current->next;
+ 
+    return lista->current->data;
 }
 
-void * lastList(List * list) {
-    return NULL;
+void * lastList(List * list) 
+{
+    if (!lista->prev) return NULL;
+    lista->current = lista->prev;
+    return list->prev->data;
+
+
 }
 
-void * prevList(List * list) {
-    return NULL;
+void * prevList(List * list) 
+{   
+    if (list->head == NULL) return NULL;
+    if (list->current == NULL) return NULL;
+    if (list->current->prev == NULL) return NULL;
+
+    list->current = list->current->prev;
+    return (list->current->data);
+
 }
 
-void pushFront(List * list, void * data) {
+void pushFront(List * list, void * data) 
+{
+    Node * nodoNuevo = createNode(data);
+    nodoNuevo->next = listaNueva->head;
+    if(listaNueva->tail == NULL)
+        listaNueva->tail = nodoNuevo;
+    listaNueva->head = nodoNuevo;
+    nodoNuevo->prev = NULL;
+}
+
 }
 
 void pushBack(List * list, void * data) {
