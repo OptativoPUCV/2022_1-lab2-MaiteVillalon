22f4516336c9651ec7715e7a77fc83ae2ccf9615
Your Name <you@example.com>
2022-03-20 01:23:42 -0300

    new try

--- list.c
+++ list.c
@@ -101,7 +101,7 @@ void pushCurrent(List * list, void * data)
         x=list->current->next;
         list->current->next=nodo2;
         nodo2->next= x;
-        //nodo2->prev=list->head; 
+        nodo2->prev=list->head; 
     }
 
 
